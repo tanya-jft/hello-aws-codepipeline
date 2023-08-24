3ec04d826e4ab8b45616b268bfb8744de5d5d71c
tanya-jft <tanya.tomar@jellyfishtechnologies.com>
2023-08-24 16:31:15 +0530

    Initial commit

---
+++ src/main/java/com/webservice/helloaws/HelloAwsApplication.java
@@ -0,0 +1,13 @@
+package com.webservice.helloaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/webservice/helloaws/HelloAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.helloaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
